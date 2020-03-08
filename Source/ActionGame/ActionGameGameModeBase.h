765dfe72aee31297770e3b0789a41f3d1719f2b3
JeongGil Lee <nainom2002@gmail.com>
2020-03-08 18:11:47 +0900

    Initialize ue4 project

---
+++ Source/ActionGame/ActionGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONGAME_API AActionGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ActionGameEditor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class ActionGameEditorTarget : TargetRules
+{
+	public ActionGameEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange( new string[] { "ActionGame" } );
+	}
+}
