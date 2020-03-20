a64300450a6baa81278e96ee58721b8bf18649b5
JeongGil Lee <nainom2002@gmail.com>
2020-03-21 04:00:26 +0900

    Input

---
+++ Source/ActionGame/Public/AGGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONGAME_API AAGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ActionGame/Public/AGPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AGCharacter.h"
+#include "AGPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONGAME_API AAGPlayerCharacter : public AAGCharacter
+{
+	GENERATED_BODY()
+	
+};
